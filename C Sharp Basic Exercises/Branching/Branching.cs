b3bf98d021fdefa58ee3957e13170ad217e3853a
Ken Shen <kshen92@gmail.com>
2018-10-26 00:59:10 -0400

    Add ExerciseP92

---
+++ C Sharp Basic Exercises/Branching/Branching.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class Branching
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your fave num?");
+            int faveNum = Convert.ToInt32(Console.ReadLine());
+
+            string result = faveNum == 12 ? "That's a dope fave number." : "What a shitty fave number.";
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+            //int currentTemp = 70;
+            //int roomTemp = 70;
+
+            //Console.WriteLine("Hi, what is your name?");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi " + name + ", what is the temperature where you are?");
+            //int currentTemperature = Convert.ToInt32(Console.ReadLine());
+
+            //if (currentTemperature == roomTemp)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemperature > roomTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");       
+            //}
+            //else if (roomTemp > currentTemperature)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("This branch is ass.");
+            //}
+
+
+            //string comparisonResult = currentTemp == roomTemp ? "It is room temperature." : "It is not room temperature.";
+            //// One-line comparison - Syntax: variable - variableName - condition - output if true - output if false
+
+            //if (currentTemp == roomTemp)
+            //{
+            //    Console.WriteLine("It is exactly room temperature.");
+            //}
+            //else if (currentTemp > roomTemp)
+            //{
+            //    Console.WriteLine("It is warmer than room temperature.");
+            //}
+            //else if (roomTemp > currentTemp)
+            //{
+            //    Console.WriteLine("It is colder than room temperature.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("It is not exactly room temperature.");
+            //}
+            //Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ C Sharp Basic Exercises/ExerciseP92/ExerciseP92.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP92
+{
+    class ExerciseP92
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below.");
+
+            bool clearFlag = false;
+            double packageWeight;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Please enter package weight:");
+                if (double.TryParse(Console.ReadLine(),out packageWeight))
+                {
+                    clearFlag = true;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid input.");
+                }
+            } while (clearFlag == false);
+
+            if (packageWeight > 50)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                System.Environment.Exit(0);
+            }
+
+            clearFlag = false;
+            double packageWidth;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Please enter package width:");
+                if (double.TryParse(Console.ReadLine(), out packageWidth))
+                {
+                    clearFlag = true;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid input.");
+                }
+            } while (clearFlag == false);
+
+            clearFlag = false;
+            double packageHeight                ;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Please enter package height:");
+                if (double.TryParse(Console.ReadLine(), out packageHeight))
+                {
+                    clearFlag = true;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid input.");
+                }
+            } while (clearFlag == false);
+
+            clearFlag = false;
+            double packageLength;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Please enter package length:");
+                if (double.TryParse(Console.ReadLine(), out packageLength))
+                {
+                    clearFlag = true;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid input.");
+                }
+            } while (clearFlag == false);
+
+            if ((packageWidth + packageHeight + packageLength) > 50)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                System.Environment.Exit(0);
+            }
+
+            double sumTotal = (packageWidth + packageHeight + packageLength) * packageWeight / 100;
+            double sumTotalRound = Math.Round(sumTotal, 2);
+
+            Console.WriteLine("");
+            Console.WriteLine("Your estimated total for shipping is $" + sumTotalRound);
+            Console.ReadLine();
+        }
+    }
+}
