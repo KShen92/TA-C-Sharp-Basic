ed9106f651af1a91c748c873b3b0f9e4a304d31d
Ken Shen <kshen92@gmail.com>
2018-10-25 23:24:41 -0400

    Add ExerciseP75

---
+++ C Sharp Basic Exercises/BooleanLogic/BooleanLogic/BooleanLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanLogic
+{
+    class BooleanLogic
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(true && false);
+            Console.WriteLine(true && true);
+            Console.WriteLine(false && false);
+
+            Console.WriteLine(true || true);
+            Console.WriteLine(true || false);
+            Console.WriteLine(false || false);
+
+            Console.WriteLine(true == true);
+            Console.WriteLine(true == false);
+            Console.WriteLine(false == false);
+
+            Console.WriteLine(true != true);
+            Console.WriteLine(true != false);
+            Console.WriteLine(false != false);
+
+            // XOR - evaluates True if one is true, but False if both or neither
+            Console.WriteLine(true ^ true);
+            Console.WriteLine(true ^ false);
+            Console.WriteLine(false ^ false);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C Sharp Basic Exercises/ExerciseP75/ExerciseP75/ExerciseP75.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP75
+{
+    class ExerciseP75
+    {
+        static void Main(string[] args)
+        {
+            bool clearFlag = false;
+            int driverAge;
+            do
+            {
+                Console.WriteLine("What is your age?");
+                if (int.TryParse(Console.ReadLine(),out driverAge))
+                {
+                    clearFlag = true;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("But use your real age though.");
+                    Console.WriteLine("");
+                }
+            } while (clearFlag == false);
+
+            string driverDUICon;
+            clearFlag = false;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Have you ever had a DUI? Please answer Yes or No.");
+                string driverDUI = Console.ReadLine();
+                driverDUICon = driverDUI.ToLower();
+                if (driverDUICon == "yes" || driverDUICon == "no")
+                {
+                    clearFlag = true;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("But use Yes or No though.");
+                }
+            } while (clearFlag == false);
+
+            int driverTickets;
+            clearFlag = false;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("How many speeding tickets do you have?");
+                if (int.TryParse(Console.ReadLine(), out driverTickets))
+                {
+                    clearFlag = true;
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("This is the police. We call bullshit.");
+                }
+            } while (clearFlag == false);
+
+            if (driverAge > 15 && driverDUICon == "no" && driverTickets <= 3)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Congratulations, you've been approved for coverage.");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.WriteLine("You didn't qualify. RIP.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
