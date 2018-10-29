a808f7107a114d11c40d19f7e1240e1c9660ff8a
Ken Shen <kshen92@gmail.com>
2018-10-28 23:20:57 -0400

    Did ExerciseP97

---
+++ C Sharp Basic Exercises/ExerciseP97/ExerciseP97.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP97
+{
+    class ExerciseP97
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tell me your name.");
+            string reply1 = "Here lies " + Console.ReadLine() + ". ";
+            Console.WriteLine("Tell me about your life.");
+            string reply2 = "He who spent his life " + Console.ReadLine() + ". ";
+            Console.WriteLine("Tell me of your greatest triumphs and failures.");
+            string reply3 = "He who most notably " + Console.ReadLine() + ".";
+
+            string replyFull = reply1 + reply2 + reply3;
+            string replyFullCaps = replyFull.ToUpper();
+
+            Console.WriteLine("");
+            Console.WriteLine(replyFull);
+            Console.WriteLine("...");
+            Console.WriteLine("I SAID, " + replyFullCaps);
+            Console.ReadLine();
+
+            StringBuilder replyPar = new StringBuilder();
+            replyPar.Append(reply1);
+            Console.WriteLine(replyPar);
+            replyPar.Replace(reply1,reply2);
+            Console.WriteLine(replyPar);
+            replyPar.Replace(reply2, reply3);
+            Console.WriteLine(replyPar);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C Sharp Basic Exercises/StringFunctions/StringFunctions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringFunctions
+{
+    class StringFunctions
+    {
+        static void Main(string[] args)
+        {
+            //changing existing strings actually results in creating a new string under the same string name - see Stringbuilder
+
+            string name = "Jesse";
+            string quote = "The man said, \"Hello\", Jesse. \n Hello on a new line. \n \t Hello on a tab.";
+            string fileName = @"C:\\Users\\Jesse";
+
+            bool trueOrFalse = name.Contains("s");
+
+            int length = name.Length;
+
+            string upperName = name.ToUpper();
+
+            Console.WriteLine(quote);
+            Console.WriteLine(fileName);
+            Console.WriteLine(trueOrFalse);
+            Console.WriteLine(length);
+            Console.WriteLine(upperName);
+            Console.ReadLine();
+
+            // useful for high volume string manipulation
+            StringBuilder sb = new StringBuilder();
+            sb.Append("My name is Ken");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
