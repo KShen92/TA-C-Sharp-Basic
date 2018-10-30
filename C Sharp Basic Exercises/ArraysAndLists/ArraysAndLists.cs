b5c82e3f0a3b5cdaf0eafe11d1819689c89524f5
Ken Shen <kshen92@gmail.com>
2018-10-29 22:56:18 -0400

    Did ExerciseP100

---
+++ C Sharp Basic Exercises/ArraysAndLists/ArraysAndLists.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysAndLists
+{
+    class ArraysAndLists
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = new int[5];
+            numArray[0] = 5;
+            numArray[1] = 2;
+            numArray[2] = 10;
+            numArray[3] = 200;
+            numArray[4] = 5000;
+
+            int[] numArray1 = new int[] { 5, 2, 10, 200, 5000 };
+
+            int[] numArray2 = { 5, 2, 10, 200, 5000 };
+
+            numArray2[5] = 650;
+
+            Console.WriteLine(numArray2[5]);
+            Console.ReadLine();
+
+            List<int> intList = new List<int>();
+            intList.Add(4);
+            intList.Add(10);
+
+            List<string> stringList = new List<string>();
+            stringList.Add("hello");
+            stringList.Add("ken");
+
+            Console.WriteLine(intList[0]);
+            Console.WriteLine(stringList[0]);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C Sharp Basic Exercises/ExerciseP100/ExerciseP100.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP100
+{
+    class ExerciseP100
+    {
+        static void Main(string[] args)
+        {
+            string[] stringArray = { "string1", "string2", "string3", "string4" };
+            int[] intArray = { 10, 20, 30, 50, 90 };
+            List<string> stringList = new List<string>();
+            stringList.Add("ah");
+            stringList.Add("yes");
+            stringList.Add("hello");
+            stringList.Add("person");
+
+            int stringArrayCount = stringArray.Count();
+            int intArrayCount = intArray.Count();
+            int stringListCount = stringList.Count();
+            bool clearFlag = false;
+            int stringArrayInd;
+            int intArrayInd;
+            int stringListInd;
+
+            do
+            {
+                Console.WriteLine("Enter an index number for your string array.");
+                if (int.TryParse(Console.ReadLine(), out stringArrayInd))
+                {
+                    if (stringArrayInd <= stringArrayCount)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Your array index value is " + stringArray[stringArrayInd]);
+                        clearFlag = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Array index does not exist.");
+                        Console.WriteLine("");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid Input");
+                    Console.WriteLine("");
+                }
+            } while (clearFlag == false);
+
+            clearFlag = false;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Enter an index number for your integer array.");
+                if (int.TryParse(Console.ReadLine(), out intArrayInd))
+                {
+                    if (intArrayInd <= intArrayCount)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Your array index value is " + intArray[intArrayInd]);
+                        clearFlag = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Array index does not exist.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid Input");
+                }
+            } while (clearFlag == false);
+
+            clearFlag = false;
+            do
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Enter an index number for your string list.");
+                if (int.TryParse(Console.ReadLine(), out stringListInd))
+                {
+                    if (stringListInd <= stringListCount)
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("Your list index value is " + stringList[stringListInd]);
+                        clearFlag = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine("List index does not exist.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Invalid Input");
+                }
+            } while (clearFlag == false);
+            Console.ReadLine();
+        }
+    }
+}
