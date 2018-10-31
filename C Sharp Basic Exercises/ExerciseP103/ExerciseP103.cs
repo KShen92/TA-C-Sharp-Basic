9847c73e430d2f0420ad08e6fd8f1cffc03424e7
Ken Shen <kshen92@gmail.com>
2018-10-30 22:05:35 -0400

    Did ExerciseP103

---
+++ C Sharp Basic Exercises/ExerciseP103/ExerciseP103.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP103
+{
+    class ExerciseP103
+    {
+        static void Main(string[] args)
+        {
+            string[] madLib = { "doge", "CHAD", "Trump", "bananas", "waluigi" };
+            Console.WriteLine("Lemme get a dope adjective.");
+            string madLibAdj = Console.ReadLine();
+            
+            for (int madLibIt = 0; madLibIt < madLib.Length; madLibIt++)
+            {
+                Console.WriteLine(madLibAdj + " " + madLib[madLibIt] + "!");
+            }
+            Console.ReadLine();
+
+            // Infinite loop:
+            //
+            //bool infinityFlag = false;
+            //while (infinityFlag == false)
+            //{
+            //    Console.WriteLine("PERFECTLY BALANCED");
+            //}
+
+            // Not-so-Infinite loop:
+            bool infinityFlag = false;
+            while (infinityFlag == false)
+            {
+                Console.WriteLine("PERFECTLY BALANCED, AS ALL THINGS SHOULD BE");
+                infinityFlag = true;
+            }
+            Console.ReadLine();
+
+            for (int compCounter1 = 0; compCounter1 < 5; compCounter1++)
+            {
+                Console.WriteLine("< comparison counter: " + compCounter1);
+            }
+            Console.ReadLine();
+
+            for (int compCounter2 = 0; compCounter2 <= 5; compCounter2++)
+            {
+                Console.WriteLine("<= comparison counter: " + compCounter2);
+            }
+            Console.ReadLine();
+
+            List<string> uniqueList = new List<string>() { "doge", "CHAD", "Trump", "bananas", "waluigi", "moardoge" };
+
+            bool lookupFlag = false;
+            bool tagFlag = false;
+            int tagCount;
+            int listIt;
+            do
+            {
+                tagCount = 0;
+                Console.WriteLine("Input search terms:");
+                string uniqueLookup = Console.ReadLine();
+                do
+                {
+                    for (listIt = 0; listIt < uniqueList.Count; listIt++)
+                    {
+                        if (uniqueList[listIt].Contains(uniqueLookup))
+                        {
+                            Console.WriteLine(uniqueLookup + " found in index " + listIt + ": " + uniqueList[listIt]);
+                            tagCount++;
+                        }
+
+                        if (listIt == (uniqueList.Count - 1) && tagCount > 0)
+                        {
+                            tagFlag = true;
+                            lookupFlag = true;
+                        }
+                        else if (listIt == (uniqueList.Count - 1) && tagCount == 0)
+                        {
+                            Console.WriteLine("Search term " + uniqueLookup + " not found.");
+                            Console.ReadLine();
+                            tagFlag = true;
+                        }
+                    }
+                } while (tagFlag == false);
+            } while (lookupFlag == false);
+            Console.ReadLine();
+
+            List<string> uniqueList2 = new List<string>() { "doge", "CHAD", "Trump", "bananas", "waluigi", "doge" };
+
+            bool lookupFlag2 = false;
+            bool tagFlag2 = false;
+            int tagCount2;
+            int listIt2;
+            do
+            {
+                tagCount2 = 0;
+                Console.WriteLine("Input search terms:");
+                string uniqueLookup2 = Console.ReadLine();
+                do
+                {
+                    for (listIt2 = 0; listIt2 < uniqueList2.Count; listIt2++)
+                    {
+                        if (uniqueList2[listIt2].Contains(uniqueLookup2))
+                        {
+                            Console.WriteLine(uniqueLookup2 + " found in index " + listIt2 + ": " + uniqueList2[listIt2]);
+                            tagCount2++;
+                        }
+
+                        if (listIt2 == (uniqueList2.Count - 1) && tagCount2 > 0)
+                        {
+                            tagFlag2 = true;
+                            lookupFlag2 = true;
+                        }
+                        else if (listIt2 == (uniqueList2.Count - 1) && tagCount2 == 0)
+                        {
+                            Console.WriteLine("Search term " + uniqueLookup2 + " not found.");
+                            Console.ReadLine();
+                            tagFlag2 = true;
+                        }
+                    }
+                } while (tagFlag2 == false);
+            } while (lookupFlag2 == false);
+            Console.ReadLine();
+
+            int repeatCounter = 0;
+            List<string> stringList = new List<string>() { "doge", "notdoge", "doge", "bush", "notdoge", "doge" };
+            List<string> stringListAlt = new List<string>();
+            foreach (string listItem in stringList)
+            {
+                Console.WriteLine("Current Item: " + listItem);
+                if (stringListAlt.Contains(listItem))
+                {
+                    Console.WriteLine("This item has already appeared in the list.");
+                }
+                stringListAlt.Add(listItem);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C Sharp Basic Exercises/Iteration/Iteration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration
+{
+    class Iteration
+    {
+        static void Main(string[] args)
+        {
+            int[] testScores = { 90, 81, 92, 83, 94, 85, 96, 87, 98, 89, 100 };
+
+            for (int i = 0; i < testScores.Length; i++)
+            {
+                if (testScores[i] >= 85)
+                {
+                    Console.WriteLine("Passing test score: " + testScores[i]);
+                }
+            }
+            Console.ReadLine();
+
+            string[] names = { "Ken", "Jesse", "Erik", "Daniel", "Adam" };
+
+            for (int j = 0; j < names.Length; j++)
+            {
+                if (names[j] == "Ken")
+                {
+                    Console.WriteLine("AH YES " + names[j] + " THE BEST NAME");
+                }
+            }
+            Console.ReadLine();
+
+            List<int> testScores2 = new List<int>();
+            testScores2.Add(75);
+            testScores2.Add(80);
+            testScores2.Add(85);
+            testScores2.Add(90);
+            testScores2.Add(95);
+            testScores2.Add(99);
+
+            foreach (int score in testScores2)
+            {
+                if (score > 85)
+                {
+                    Console.WriteLine("Passing test score: " + score);
+                }
+            }
+            Console.ReadLine();
+
+            List<string> names2 = new List<string>() { "Ken", "Jesse", "Erik", "Adam", "Daniel" };
+
+            foreach (string name in names2)
+            {
+                    Console.WriteLine(name);
+            }
+            Console.ReadLine();
+
+            List<int> testScores3 = new List<int>() { 75, 80, 85, 90, 95 };
+            List<int> passingScores = new List<int>();
+
+            foreach (int score in testScores3)
+            {
+                if (score >= 85)
+                {
+                    passingScores.Add(score);
+                }
+            }
+            Console.WriteLine("There are " + passingScores.Count + " passing scores.");
+            passingScores.ForEach(Console.WriteLine);
+            Console.ReadLine();
+        }
+    }
+}
