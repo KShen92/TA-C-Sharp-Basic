810232f221045e64c71ab18744625bd50febe4c9
Ken Shen <kshen92@gmail.com>
2018-11-02 00:11:24 -0400

    Did ExerciseP111

---
+++ C Sharp Basic Exercises/ExerciseP111/ExerciseP111.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP111
+{
+    class ExerciseP111
+    {
+        static void Main(string[] args)
+        {
+            bool clearFlag = false;
+            int intNum = 0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter an integer to get fucking handled:");
+                    intNum = Convert.ToInt32(Console.ReadLine());
+                    clearFlag = true;
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine(error.Message);
+                    Console.ReadLine();
+                }
+            } while (clearFlag == false);
+
+            MathOperations calcs = new MathOperations();
+
+            int result1 = MathOperations.Operation1(intNum);
+            int result2 = MathOperations.Operation2(intNum);
+            int result3 = MathOperations.Operation3(intNum);
+
+            Console.WriteLine("Your integer is " + intNum);
+            Console.WriteLine(intNum + " multiplied by 2 is " + result1);
+            Console.WriteLine(intNum + " plus 10 is " + result2);
+            Console.WriteLine(intNum + " minus 5 is " + result3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C Sharp Basic Exercises/ExerciseP111/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP111
+{
+    public class MathOperations
+    {
+        public static int Operation1(int intNum)
+        {
+            int result1 = intNum * 2;
+            return result1;
+        }
+
+        public static int Operation2(int intNum)
+        {
+            int result2 = intNum + 10;
+            return result2;
+        }
+
+        public static int Operation3(int intNum)
+        {
+            int result3 = intNum - 5;
+            return result3;
+        }
+    }
+}
