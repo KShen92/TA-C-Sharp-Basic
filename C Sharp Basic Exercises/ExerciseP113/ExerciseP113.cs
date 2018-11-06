b2421855858d09b786ae30b86f515b81ff69e76d
Ken Shen <kshen92@gmail.com>
2018-11-06 00:28:34 -0500

    Did ExerciseP113

---
+++ C Sharp Basic Exercises/ExerciseP113/ExerciseP113.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP113
+{
+    class ExerciseP113
+    {
+        static void Main(string[] args)
+        {
+            MathOps mathOps = new MathOps();
+            mathOps.MathOps1(int1: 4, int2: 5);
+        }
+    }
+}
---
+++ C Sharp Basic Exercises/ExerciseP113/MathOps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP113
+{
+    public class MathOps
+    {
+        public void MathOps1(int int1, int int2)
+        {
+            int int1Op = int1 * 2;
+            Console.WriteLine("Your first integer, " + int1 + ", times 2, is " + int1Op);
+            Console.WriteLine("Your second integer was " + int2);
+            Console.ReadLine();
+        }
+    }
+}
