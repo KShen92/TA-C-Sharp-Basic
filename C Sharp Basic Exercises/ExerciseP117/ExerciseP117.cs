d84c268242c8789313490146061eac4ff0763b26
Ken Shen <kshen92@gmail.com>
2018-11-09 00:28:24 -0500

    Did ExerciseP117

---
+++ C Sharp Basic Exercises/ExerciseP117/ExerciseP117.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP117
+{
+    class ExerciseP117
+    {
+        static void Main(string[] args)
+        {
+            bool clearFlag = false;
+            double num1 = 0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Enter a number:");
+                    num1 = Convert.ToDouble(Console.ReadLine());
+                    clearFlag = true;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid Input");
+                }
+            } while (clearFlag == false);
+
+            MathOps calc = new MathOps();
+            int result1 = 0;
+            calc.MathOps1(out result1, num1);
+
+            Console.WriteLine(num1 + " divided by two and rounded equals " + result1);
+            Console.ReadLine();
+
+            int num2 = 2;
+            double result2 = 0;
+            calc.MathOps1(out result2, num1, num2);
+
+            Console.WriteLine(num1 + " times " + num2 + " equals " + result2);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C Sharp Basic Exercises/ExerciseP117/MathOps.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP117
+{
+    public class MathOps
+    {
+        public void MathOps1(out int result1, double num1)
+        {
+            double temp1 = num1 / 2;
+            result1 = Convert.ToInt32(temp1);
+        }
+
+        public void MathOps1(out double result2, double num1, int num2)
+        {
+            result2 = num1 * num2;
+        }
+    }
+}
