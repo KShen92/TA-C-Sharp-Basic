bf174e3b45a843d2dfa19e13544034513db31330
Ken Shen <kshen92@gmail.com>
2018-11-12 23:56:57 -0500

    Work ExerciseP124

---
+++ C Sharp Basic Exercises/ExerciseP124/ExerciseP124.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP124
+{
+    class ExerciseP124
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.firstName = "Sample";
+            emp1.lastName = "Student";
+            emp1.SayName();
+        }
+    }
+}
