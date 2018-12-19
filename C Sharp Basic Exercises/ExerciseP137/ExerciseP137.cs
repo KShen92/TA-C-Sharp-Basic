e7af33480d7670430620e0617d84fd244b37303e
Ken Shen <kshen92@gmail.com>
2018-12-18 23:22:22 -0500

    Did ExerciseP137

---
+++ C Sharp Basic Exercises/ExerciseP137/ExerciseP137.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP137
+{
+    class ExerciseP137
+    {
+        static void Main(string[] args)
+        {
+            Number num1 = new Number();
+            num1.Amount = 12.5;
+            Console.WriteLine(num1.Amount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C Sharp Basic Exercises/ExerciseP137/Number.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP137
+{
+    public class Number
+    {
+        public double Amount = 0;
+    }
+}
