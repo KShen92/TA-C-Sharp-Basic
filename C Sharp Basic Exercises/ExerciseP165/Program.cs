a1e074e9485edbded6257823505722080c73fd7e
Ken Shen <kshen92@gmail.com>
2019-02-10 15:51:07 -0500

    ExerciseP165
    WIP

---
+++ C Sharp Basic Exercises/ExerciseP165/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP165
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validInput = false;
+            int userAge = 0;
+
+            while (!validInput)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter your age:");
+                    validInput = int.TryParse(Console.ReadLine(), out userAge);
+                    if (userAge <= 0)
+                    {
+                        throw new ZeroNegException();
+                    }
+                }
+                catch (ZeroNegException)
+                {
+                    Console.WriteLine("Please enter an age greater than 0.");
+                    validInput = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input.");
+                    validInput = false;
+                }
+            }
+
+            int currentYear = DateTime.Now.Year;
+            int birthYear = currentYear - userAge;
+            Console.WriteLine("Your birth year is {0}.", birthYear);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C Sharp Basic Exercises/ExerciseP165/ZeroNegException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseP165
+{
+    public class ZeroNegException : Exception
+    {
+        public ZeroNegException()
+    :       base() { }
+        public ZeroNegException(string message)
+    :       base(message) { }        
+    }
+}
