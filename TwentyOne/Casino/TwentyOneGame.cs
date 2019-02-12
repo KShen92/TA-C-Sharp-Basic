7c398e9006c1634b375612e36b767493eca8d5c7
Ken Shen <kshen92@gmail.com>
2019-02-11 22:27:10 -0500

    TwentyOne Update
    +SQL logging

--- TwentyOne/Casino/TwentyOneGame.cs
+++ TwentyOne/Casino/TwentyOneGame.cs
@@ -36,7 +36,7 @@ namespace Casino.TwentyOne
                 }
                 if (bet < 0)
                 {
-                    throw new FraudException();
+                    throw new FraudException("Security removed this individual.");
                 }
                 bool successfullyBet = player.Bet(bet);
                 if (!successfullyBet)   // if (successfullyBet == false)
