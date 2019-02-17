a48751b5b8b91e74523bbc1a1690c29e9322635a
Ken Shen <kshen92@gmail.com>
2019-02-16 21:54:35 -0500

    MVC Tutorial/App Progress

--- Web Apps/MVC Tutorial/MVC Tutorial/Controllers/HomeController.cs
+++ Web Apps/MVC Tutorial/MVC Tutorial/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC_Tutorial.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,19 +11,49 @@ namespace MVC_Tutorial.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            //string text = "Hello";
+            //System.IO.File.WriteAllText(@"D:\Tech Academy\TA-C-Sharp-Basic\Web Apps\MVC Tutorial\log.txt", text);
+
+            //Random rnd = new Random(10);
+            //int num = rnd.Next();
+
+            //if (num > 20000)
+            //{
+            //    return View("About");
+            //}
+
+            //return Content("Hello"); - plain text content
+
+            //return RedirectToAction("Contact"); - redirect to other view
+
+            //List<string> names = new List<String>
+            //{
+            //    "Jesse",
+            //    "Ken",
+            //    "Clayton"
+            //};
+
+            User user = new User();
+            user.Id = 1;
+            user.FirstName = "Ken";
+            user.LastName = "Shen";
+            user.Age = 26;
+
+            return View(user);
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
+            //throw new Exception("Invalid page"); - creates error page
+
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contact(int id = 0)
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = id;
 
             return View();
         }
