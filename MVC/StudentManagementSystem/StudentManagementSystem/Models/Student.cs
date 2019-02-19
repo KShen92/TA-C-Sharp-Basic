d5ec761a624a93d0bbc3f97b1c77692d43a4ee74
Ken Shen <kshen92@gmail.com>
2019-02-18 22:53:47 -0500

    SMS MVC Progress

--- MVC/StudentManagementSystem/StudentManagementSystem/Models/Student.cs
+++ MVC/StudentManagementSystem/StudentManagementSystem/Models/Student.cs
@@ -10,5 +10,6 @@ namespace StudentManagementSystem.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
     }
 }
